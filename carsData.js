8c75257817ca4a5a7c2f254d87e40beb40e97eed
funky86 <tilen.bobek@gmail.com>
2024-03-30 13:28:45 +0100

    Add files via upload

---
+++ carsData.js
@@ -0,0 +1,38 @@
+const carsData = [
+  {
+    "label": "Tesla Model 3",
+    "usableBattery": 57.5,
+    "realRange": 415,
+    "consumptionAverage": 139,
+    "consumptionColdWeatherCity": 149,
+    "consumptionColdWeatherHighway": 192,
+    "consumptionColdWeatherCombined": 167,
+    "consumptionMildWeatherCity": 94,
+    "consumptionMildWeatherHighway": 144,
+    "consumptionMildWeatherCombined": 117
+  },
+  {
+    "label": "Tesla Model 3 Long Range Dual Motor",
+    "usableBattery": 75,
+    "realRange": 500,
+    "consumptionAverage": 150,
+    "consumptionColdWeatherCity": 161,
+    "consumptionColdWeatherHighway": 208,
+    "consumptionColdWeatherCombined": 181,
+    "consumptionMildWeatherCity": 104,
+    "consumptionMildWeatherHighway": 156,
+    "consumptionMildWeatherCombined": 128
+  },
+  {
+    "label": "Audi Q4 Sportback e-tron 45",
+    "usableBattery": 77,
+    "realRange": 435,
+    "consumptionAverage": 177,
+    "consumptionColdWeatherCity": 181,
+    "consumptionColdWeatherHighway": 244,
+    "consumptionColdWeatherCombined": 208,
+    "consumptionMildWeatherCity": 121,
+    "consumptionMildWeatherHighway": 190,
+    "consumptionMildWeatherCombined": 154
+  }
+];
